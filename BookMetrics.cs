1db10f35da60e26ebd8d2f218265339a5d814d08
Ryan McCauley <ryanmccauley211@gmail.com>
2018-08-13 15:01:28 +0100

    - Add BookMetrics class

---
+++ BookMetrics.cs
@@ -0,0 +1,39 @@
+﻿namespace PrickleParser{
+    
+    public class BookMetrics{
+        private int nbPages;
+        private string author;
+        private string title;
+        private string date;
+        private string publisher;
+
+        public BookMetrics(){
+            
+        }
+
+        public int NbPages{
+            get => nbPages;
+            set => nbPages = value;
+        }
+
+        public string Author{
+            get => author;
+            set => author = value;
+        }
+
+        public string Title{
+            get => title;
+            set => title = value;
+        }
+
+        public string Date{
+            get => date;
+            set => date = value;
+        }
+
+        public string Publisher{
+            get => publisher;
+            set => publisher = value;
+        }
+    }
+}
