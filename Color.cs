231e0c0f56021055262047885991adef7be80ec4
Ryan McCauley <ryanmccauley211@gmail.com>
2018-08-13 15:01:15 +0100

    - Add Color class

---
+++ Color.cs
@@ -0,0 +1,38 @@
+﻿namespace PrickleParser{
+    public class Color{
+        private int MAX_VALUE = 255;
+        private ColorFormat colorFormat;
+
+        private int graylevel;
+        
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
+        
+        private enum ColorFormat{
+            grayscale = 0,
+            rgb       = 1
+        }
+
+        public Color(int graylevel){
+            this.graylevel = graylevel;
+            this.colorFormat = ColorFormat.grayscale;
+        }
+
+        public Color(int red, int green, int blue, int alpha){
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+            this.colorFormat = ColorFormat.rgb;
+        }
+        
+        public Color(int red, int green, int blue){
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = MAX_VALUE;
+        }
+    }
+}
