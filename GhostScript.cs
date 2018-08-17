7dd41f14467e2fc084f9fb70826a984ce95be88e
Ryan McCauley <ryanmccauley211@gmail.com>
2018-08-17 10:44:45 +0100

    - Add ghostscript for removing pdf text

---
+++ GhostScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+
+namespace PrickleParser{
+    public class GhostScript{
+        
+        private static string GS_PATH = Path.Combine(
+            Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "Lib", "gs");
+        private static string OUTPUT_DIR = Path.GetTempPath();
+
+        public static void RemoveText(string input){
+            
+            if (File.Exists(input)){
+                var p = new Process();
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.FileName = GS_PATH;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.RedirectStandardOutput = true;
+
+                p.StartInfo.Arguments = "-o " + Path.Combine(OUTPUT_DIR, "no_text_" + Path.GetFileName(input)) + 
+                                        " -sDEVICE=pdfwrite -dFILTERTEXT " + input;
+
+                p.Start();
+
+                Console.Write(p.StandardError.ReadToEnd()); // todo log error
+                Console.Write(p.StandardOutput.ReadToEnd()); // todo log info
+
+                p.WaitForExit(); 
+            }
+            else{
+                Console.WriteLine("no file"); // todo log error
+            }
+        }
+        
+        
+    }
+}
