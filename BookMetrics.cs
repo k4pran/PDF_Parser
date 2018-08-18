a4ca15ddad924036414dea7b19644c9a2541b6c6
Ryan McCauley <ryanmccauley211@gmail.com>
2018-08-18 16:24:36 +0100

    - Add json.net for reading / writing books to json file

--- BookMetrics.cs
+++ BookMetrics.cs
@@ -1,6 +1,14 @@
-﻿namespace PrickleParser{
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using Newtonsoft.Json;
+
+namespace PrickleParser{
     
     public class BookMetrics{
+
+        private List<PageMetrics> pages;
         private int nbPages;
         private string author;
         private string title;
@@ -8,13 +16,65 @@
         private string publisher;
 
         public BookMetrics(){
-            
+            pages = new List<PageMetrics>();
+            nbPages = 0;
+            Author = "";
+            title = "";
+            date = "";
+            publisher = "";
         }
 
-        public int NbPages{
-            get => nbPages;
-            set => nbPages = value;
+        public void AddPage(PageMetrics page){
+            pages.Add(page);
+            nbPages++;
         }
+
+        public void ToJson(string output){
+            try{
+                File.WriteAllText(output, JsonConvert.SerializeObject(this, Formatting.Indented));
+            }
+            catch(Exception e){
+                if (e is UnauthorizedAccessException || e is SecurityException){
+                    e.Data.Add("WriteJsonError", "Failed to write json to path: " + output + 
+                                                 " as you do not have the required permissions");
+                    throw;
+                }
+                if (e is NotSupportedException){
+                    e.Data.Add("WriteJsonError", "Failed to write json to path: " + output +
+                                                 " as path is not in a valid format");
+                    throw;
+                }
+                e.Data.Add("WriteJsonError", "Failed to write json to path: " + output);
+                throw;
+            }
+        }
+
+        public static BookMetrics FromJson(string input){
+            if (File.Exists(input)){
+                try{
+                    return JsonConvert.DeserializeObject<BookMetrics>(File.ReadAllText(input));
+                }
+                catch(Exception e){
+                    if (e is UnauthorizedAccessException || e is SecurityException){
+                        e.Data.Add("ReadJsonError", "Failed to read json to path: " + input + 
+                                                     " as you do not have the required permissions");
+                        throw;
+                    }
+                    if (e is NotSupportedException){
+                        e.Data.Add("ReadJsonError", "Failed to read json to path: " + input +
+                                                     " as path is not in a valid format");
+                        throw;
+                    }
+                    e.Data.Add("ReadJsonError", "Failed to read json data from path: " + input);
+                    throw;                    
+                }
+            }
+            throw new Exception("Input file not found at path: " + input);
+        }
+
+        public List<PageMetrics> Pages => pages;
+
+        public int NbPages => nbPages;
 
         public string Author{
             get => author;
