0bf4d6554ffef5963938beaca04fb4a89c95a066
Ryan McCauley <ryanmccauley211@gmail.com>
2018-08-23 18:51:11 +0100

    - Add metrices for words and chars to page. - Include line left and right positions. - Override toString()

--- LineMetrics.cs
+++ LineMetrics.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PrickleParser{
     public class LineMetrics{
@@ -7,24 +9,26 @@ namespace PrickleParser{
         private float descent;
         private float lineLineSpacingAbove;
         private float lineLineSpacingBelow;
-        private List<WordMetrics> words;
+        private float leftMostPos;
+        private float rightMostPos;
+        private List<ChunkMetrics> wordMetrices;
 
         public LineMetrics(float ascent, float baseline, float descent){
-            this.words = new List<WordMetrics>();
+            wordMetrices = new List<ChunkMetrics>();
             this.ascent = ascent;
             this.baseline = baseline;
             this.descent = descent;
         }
 
-        public void AddWords(List<WordMetrics> words){
-            this.words.AddRange(words);
+        public void AddWords(List<ChunkMetrics> words){
+            this.wordMetrices.AddRange(words);
         }
 
-        public void AddWord(WordMetrics word){
-            this.words.Add(word);
+        public void AddWord(ChunkMetrics chunk){
+            this.wordMetrices.Add(chunk);
         }
 
-        public List<WordMetrics> Words => words;
+        public List<ChunkMetrics> WordMetrices => wordMetrices;
 
         public float Ascent{
             get => ascent;
@@ -51,6 +55,24 @@ namespace PrickleParser{
             set => lineLineSpacingBelow = value;
         }
 
+        public float LeftMostPos{
+            get => leftMostPos;
+            set => leftMostPos = value;
+        }
+
+        public float RightMostPos{
+            get => rightMostPos;
+            set => rightMostPos = value;
+        }
+
+        public float ContentHeight(){
+            return Math.Abs(ascent - descent);
+        }
+        
+        public float ContentWidth(){
+            return Math.Abs(RightMostPos - LeftMostPos);
+        }
+        
         public override bool Equals(object obj){
             if (obj == null){
                 return false;
@@ -65,6 +87,10 @@ namespace PrickleParser{
             return this.baseline == toCompare.baseline;
         }
 
+        public override string ToString(){
+            return String.Join(" ", WordMetrices.Select(x => x.ToString()).ToArray());
+        }
+        
         public override int GetHashCode(){
             int hash = 13;
             hash = (hash * 7) + baseline.GetHashCode();
