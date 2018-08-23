81a5dff9ef398485da07be0af103009c4cf8676f
Ryan McCauley <ryanmccauley211@gmail.com>
2018-08-23 18:50:22 +0100

    - Add metrices for words and chars to page. - Include line left and right positions.

--- PageMetrics.cs
+++ PageMetrics.cs
@@ -7,8 +7,10 @@ namespace PrickleParser{
     public class PageMetrics{
 
         private int pageNumber;
-        private List<LineMetrics> lines;
-        private List<WordMetrics> words;
+        private List<LineMetrics> lineMetrices;
+        private List<ChunkMetrics> chunkMetrices;
+        private List<WordMetrics> wordMetrices;
+        private List<CharMetrics> charMetrices;
         private string text;
         private float width;
         private float height;
@@ -17,59 +19,64 @@ namespace PrickleParser{
 
         public PageMetrics(int pageNumber){
             this.pageNumber = pageNumber;
-            this.lines = new List<LineMetrics>();
-            this.words = new List<WordMetrics>();
-            this.text = "";
+            lineMetrices = new List<LineMetrics>();
+            chunkMetrices = new List<ChunkMetrics>();
+            wordMetrices = new List<WordMetrics>();
+            charMetrices = new List<CharMetrics>();
+            
+            text = "";
         }
 
-        public void AddWord(WordMetrics word){
-            words.Add(word);
+        public void AddWord(ChunkMetrics chunk){
+            chunkMetrices.Add(chunk);
         }
 
         public void BuildLines(){
-            IDictionary<LineMetrics, List<WordMetrics>> lineMappings = new Dictionary<LineMetrics, List<WordMetrics>>();
-            foreach(WordMetrics wordMetric in words){
+            IDictionary<LineMetrics, List<ChunkMetrics>> lineMappings = new Dictionary<LineMetrics, List<ChunkMetrics>>();
+            
+            foreach(ChunkMetrics wordMetric in chunkMetrices){
                 LineMetrics line = new LineMetrics(wordMetric.Ascent, wordMetric.Baseline, wordMetric.Descent);
                 if (lineMappings.ContainsKey(line)){
                     lineMappings[line].Add(wordMetric);
-                    
                 }
                 else{
-                    lineMappings.Add(line, new List<WordMetrics>{wordMetric});
+                    lineMappings.Add(line, new List<ChunkMetrics>{wordMetric});
                 }
             }
-
-            foreach(KeyValuePair<LineMetrics, List<WordMetrics>> entry in lineMappings){
+            
+            foreach(KeyValuePair<LineMetrics, List<ChunkMetrics>> entry in lineMappings){
                 entry.Key.AddWords(entry.Value);
-                lines.Add(entry.Key);
+                entry.Key.LeftMostPos = entry.Value.ElementAt(0).BottomLeft.X;
+                entry.Key.RightMostPos = entry.Value.ElementAt(entry.Value.Count - 1).BottomRight.X;
+                lineMetrices.Add(entry.Key);
             }
         }
 
         public void DetermineLineSpacing(){
-            if (lines.Count == 0){
+            if (lineMetrices.Count == 0){
                 avgLineSpacing = -1;
                 return;
             }
 
-            if (lines.Count == 1){
+            if (lineMetrices.Count == 1){
                 avgLineSpacing = -1;
-                lines[0].LineSpacingAbove = -1;
-                lines[0].LineSpacingBelow = -1;
+                lineMetrices[0].LineSpacingAbove = -1;
+                lineMetrices[0].LineSpacingBelow = -1;
                 return;
             }
             
             float[] spacings = {};
 
-            float lastDescent = lines[0].Descent;
-            lines[0].LineSpacingAbove = -1;
-            for(int i = 1; i < lines.Count; i++){
-                float spacing = lastDescent - lines[i].Ascent;
-                lines[i].LineSpacingAbove = spacing;
-                lines[i - 1].LineSpacingBelow = spacing;
-                lastDescent = lines[i].Descent;
+            float lastDescent = lineMetrices[0].Descent;
+            lineMetrices[0].LineSpacingAbove = -1;
+            for(int i = 1; i < lineMetrices.Count; i++){
+                float spacing = lastDescent - lineMetrices[i].Ascent;
+                lineMetrices[i].LineSpacingAbove = spacing;
+                lineMetrices[i - 1].LineSpacingBelow = spacing;
+                lastDescent = lineMetrices[i].Descent;
                 spacings.Append(spacing);
             }
-            lines[lines.Count - 1].LineSpacingBelow = -1;
+            lineMetrices[lineMetrices.Count - 1].LineSpacingBelow = -1;
 
             float sum = 0f;
             for(int i = 0; i < spacings.Length; i++){
@@ -79,10 +86,10 @@ namespace PrickleParser{
         }
 
         public void PrintLines(){
-            foreach(LineMetrics line in lines){
+            foreach(LineMetrics line in lineMetrices){
                 StringBuilder sb = new StringBuilder();
-                foreach(WordMetrics wordMetric in line.Words){
-                    sb.Append(wordMetric.Word);
+                foreach(ChunkMetrics wordMetric in line.WordMetrices){
+                    sb.Append(wordMetric.WordStr);
                     sb.Append(" ");
                 }
                 Console.WriteLine(sb.ToString());
@@ -99,14 +106,24 @@ namespace PrickleParser{
             set => text = value;
         }
 
-        public List<LineMetrics> Lines{
-            get => lines;
-            set => lines = value;
+        public List<LineMetrics> LineMetrices{
+            get => lineMetrices;
+            set => lineMetrices = value;
         }
 
-        public List<WordMetrics> Words{
-            get => words;
-            set => words = value;
+        public List<ChunkMetrics> ChunkMetrices{
+            get => chunkMetrices;
+            set => chunkMetrices = value;
+        }
+
+        public List<WordMetrics> WordMetrices{
+            get => wordMetrices;
+            set => wordMetrices = value;
+        }
+
+        public List<CharMetrics> CharMetrices{
+            get => charMetrices;
+            set => charMetrices = value;
         }
 
         public float Width{
